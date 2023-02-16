3e2bbaaecad9e829df0bda52fcfe3e1dcaa1085f
cephalopod <adamhannaford1@gmail.com>
2023-02-16 08:25:05 +0000

    add getWeather function and call from search bar

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,16 @@
-//html elements
+
+
+// testing params
+city = "london";
+
+
+
+// HTML ELEMENTS
 var searchInput = $("#search-input");
 var searchButton = $("#search-button");
 var todayPanel = $("#today");
 var forecastPanel = $("#forecast");
+
 
 
 // CITY SEARCH BAR
@@ -10,15 +18,15 @@ searchButton.on("click", function (event) {
     event.preventDefault();
     var city = searchInput.val();
     console.log(city);
+    getWeather(city);
 })
 
 
-
-// WEATHER API SETTINGS
+function getWeather(city) {
+// WEATHER API CALL
 var apiKey = "64f996f6510a1f1f557ed8a96abace4a";
 var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
-// API CALL
 $.ajax({
     url: queryURL,
     method: "GET"
@@ -30,5 +38,34 @@ $.ajax({
     var today = moment().format("D MMM YYYY");
     console.log(today);
 
+    // GET WEATHER FROM RESPONSE
+    var output = {
+        name: response.city.name,
+        coordinates: response.city.coord,
+        description: response.list[0].weather[0].description,
+        temperature: response.list[0].main.temp,
+        windAngle: response.list[0].wind.deg,
+        windSpeed: response.list[0].wind.speed,
+        windGust: response.list[0].wind.gust,
+        humidity: response.list[0].main.humidity
+        };
+        console.log(output.name);
+        console.log(output.coordinates);
+        console.log(output.description);
+        console.log(output.temperature);
+        console.log(output.windAngle);
+        console.log(output.windSpeed);
+        console.log(output.windGust);
+        console.log(output.humidity);
+    })
 
-})
+
+
+
+
+
+
+    
+
+};
+// end of getWeather function
