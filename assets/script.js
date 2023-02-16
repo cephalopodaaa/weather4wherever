12b7f6583c7b94f44690ff3cf52680cbe07cf25f
cephalopod <adamhannaford1@gmail.com>
2023-02-16 06:02:14 +0000

    ajax call and html elements

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,25 @@
 
+//testing variables
+var city = "london";
 
 
+//html elements
+var searchInput = $("#search-input");
+var searchButton = $("#search-button")
+var today = $("#today");
+var forecast = $("#forecast");
+
+
+//weather API
 var apiKey = "64f996f6510a1f1f557ed8a96abace4a";
+var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+})
